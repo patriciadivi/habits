3e2a7c0ba867668d938f30d78840e423f7df3355
PatriciaDivi <patriciavianad@gmail.com>
2023-02-04 21:52:33 -0300

    feat(Desenvolvimento): :sparkles: Implementando componente 'Header'

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,8 +2,18 @@ import logoImage from "../assets/logo.svg";
 
 export function Header() {
   return (
-    <div>
-      <h1>Habits</h1>
+    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+      <img
+        src={logoImage}
+        alt="logo de habits, quadrados superiores em varios tons de roxo e, texto habits abaixo do quadrados no tom de roxo escuro"
+      />
+
+      <button
+        type="button"
+        className="border border-violet-500 font-semibold rounded-lg px-6 py-4"
+      >
+        Novo hábito
+      </button>
     </div>
   );
 }

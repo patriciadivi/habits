b26518b8f6fbc72bc97fe80da9a9da86fc927bb9
PatriciaDivi <patriciavianad@gmail.com>
2023-02-05 04:24:21 -0300

    docs(Desenvolvimento): :wrench: Configuração tipagem para o 'tailwind' reconhecer o 'className'

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 export function Home() {
   return (
-    <View>
+    <View className="flex-1 bg-background px-8 pt-16">
       <Text>CHEGUEIIIII</Text>
     </View>
   );

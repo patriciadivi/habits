d06708e4aae45f9a70412f6caf96b01bc499c6c2
PatriciaDivi <patriciavianad@gmail.com>
2023-02-09 17:44:08 -0300

    feat(Desenvolvimento): :sparkles: Implementado botão para o retorno da pagina anterior

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,16 @@
-import { View } from "react-native"
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function New() {
-    return (
-        <View className="flex-1 bg-background px-8 pt16 ">
-            
-        </View>
-    )
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+      </ScrollView>
+    </View>
+  )
 }

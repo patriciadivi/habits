4059c8648629697f30850894b24b5d123b713688
PatriciaDivi <patriciavianad@gmail.com>
2023-02-09 21:18:38 -0300

    feat(Desenvolvimento): :sparkles: Componente "New"  desenvolvido para criar novos hábitos

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,23 @@
-import { ScrollView, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
 
 import { BackButton } from "../components/BackButton";
-import { Checkbox } from "../components/CheckBox";
+import { Checkbox } from "../components/Checkbox";
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export function New() {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
+    } else {
+      setWeekDays(prevState => [...prevState, weekDayIndex])
+    }
+  }
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView 
@@ -22,10 +36,39 @@ export function New() {
 
         <TextInput 
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
         />
 
-        <Checkbox title="Beber 2L de água" />
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          Qual a recorrência?
+        </Text>
 
+        {
+          availableWeekDays.map((weekDay, index) => (
+            <Checkbox
+              key={weekDay}
+              title={weekDay}
+              checked={weekDays.includes(index)}
+              onPress={() => handleToggleWeekDay(index)}
+            />
+          ))
+        }
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather 
+           name="check"
+           size={20}
+           color={colors.white}
+          />
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   )

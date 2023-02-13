c72f491cba2c2f11ca9be0e90da36c5ca595ad5c
PatriciaDivi <patriciavianad@gmail.com>
2023-02-13 16:26:28 -0300

    style(Desenvolvimento): :lipstick: Corrigindo algumas estilizações na aplicação

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -21,19 +21,17 @@ export function NewHabitForm() {
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-      <Checkbox.Root
-              className='flex items-center gap-3 group'
-            >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-              <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
+        <Checkbox.Root className='flex items-center gap-3 group'>
+          <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+          <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+          </div>
 
-              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
+          <span className='text-white leading-tight'>
+            Segunda-feira
+          </span>
+        </Checkbox.Root>
       </div>
 
         <button 

f1658a2c9203d4db7664ffaef798e79468a0b308
PatriciaDivi <patriciavianad@gmail.com>
2023-03-01 18:05:59 -0300

    style(Desenvolvimento): :lipstick: Implementando animações no 'Checkbox'

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -1,6 +1,12 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
-import { Feather } from '@expo/vector-icons'
-import colors from "tailwindcss/colors";
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated';
 
 interface Props extends TouchableOpacityProps {
   title: string;
@@ -11,25 +17,24 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      className="flex-row mb-2 items-center"
-      {...rest}
-    >
-      { checked 
-        ?
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-          <Feather 
-            name="check"
+      className='flex-row mb-2 items-center'
+      {...rest}>
+      {checked ? (
+        <Animated.View
+          className='h-8 w-8 bg-green-500 rounded-lg items-center justify-center'
+          entering={ZoomIn}
+          exiting={ZoomOut}>
+          <Feather
+            name='check'
             size={20}
             color={colors.white}
           />
-        </View>
-        : 
-        <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
-      }
+        </Animated.View>
+      ) : (
+        <View className='h-8 w-8 bg-zinc-900 rounded-lg' />
+      )}
 
-      <Text className="text-white text-base ml-3 font-semibold">
-        {title}
-      </Text>
+      <Text className='text-white text-base ml-3 font-semibold'>{title}</Text>
     </TouchableOpacity>
-  )
+  );
 }
